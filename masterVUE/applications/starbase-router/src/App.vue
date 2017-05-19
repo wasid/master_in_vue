69a1e5457df8e15244b32cf96aec203ed5303407
Wasid Hossain <wasidmail@gmail.com>
2017-05-19 17:41:54 +0000

    Router-Linking Different Pages

--- masterVUE/applications/starbase-router/src/App.vue
+++ masterVUE/applications/starbase-router/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
     <h1>Startbase Router</h1>
+    <h4>
+      <router-link to="/data/people">Charecters</router-link> |
+      <router-link to="/data/planets">Planets</router-link>
+    </h4>
+    <router-view></router-view>
   </div>
 </template>
---
+++ masterVUE/applications/starbase-router/src/components/Data.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="">
+    Type: {{type}}    
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return {
+            type: this.$route.params.type
+        }
+    },
+    watch: {
+        '$route': 'tweak'
+    },
+    methods: {
+        tweak(){
+            this.type = this.$route.params.type
+        }
+    }
+}
+    
+</script>
