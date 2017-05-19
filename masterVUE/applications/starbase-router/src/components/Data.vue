433637983ff6cfd9181873853642da8fd1acbbde
Wasid Hossain <wasidmail@gmail.com>
2017-05-19 18:25:30 +0000

    Developing the Data Component

--- masterVUE/applications/starbase-router/src/components/Data.vue
+++ masterVUE/applications/starbase-router/src/components/Data.vue
@@ -1,23 +1,39 @@
 <template>
     <div class="">
-    Type: {{type}}    
+    {{items}}    
     </div>
 </template>
 <script>
 export default{
     data(){
         return {
-            type: this.$route.params.type
-        }
+            type: this.$route.params.type,
+            items: []
+        };
     },
     watch: {
         '$route': 'tweak'
     },
     methods: {
         tweak(){
-            this.type = this.$route.params.type
+            this.items = [];
+            this.type = this.$route.params.type;
+            let initial_ids = [1,3,4,5,6,7];
+            
+            for (let i in initial_ids) {
+                let id = initial_ids[i];
+                fetch(`http://swapi.co/api/${this.type}/${id}`, {
+                method:"GET"
+              })
+              .then(respose => respose.json())
+              .then(json => this.items.push(json));
+            }
         }
+    },
+        
+    created(){
+        this.tweak();
     }
-}
+};
     
 </script>
